e6c9f50580d25a9d18e012d3eb47c7d67520b0d1
kurowski.ch <kurowski.isa@gmail.com>
2018-10-29 15:37:47 +0100

    EIB-001: Szablon projektu.

---
+++ src/main/java/com/ctk/servlet/IndexServlet.java
@@ -0,0 +1,41 @@
+package com.ctk.servlet;
+
+import com.ctk.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/")
+public class IndexServlet extends HttpServlet {
+
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Inject
+    private ModelGeneratorTemplate modelGeneratorTemplate;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setHeader("Content-Type", "text/html; charset=UTF-8");
+        resp.setContentType("text/html;charset=UTF-8 pageEncoding=\"UTF-8");
+
+        try {
+            Template template = templateProvider.getTemplate(getServletContext(), "indexTemplate");
+
+            template.process(modelGeneratorTemplate.getModel(), resp.getWriter());
+
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/ctk/servlet/ModelGeneratorTemplate.java
@@ -0,0 +1,20 @@
+package com.ctk.servlet;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped
+public class ModelGeneratorTemplate {
+
+    private Map<String, Object> model = new HashMap<>();
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(String s, Object o) {
+        this.model.put(s, o);
+    }
+
+}
