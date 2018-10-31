1144f0257218accb1c17f862719a4a03f05fe443
kurowskicisa <kurowskic.isa@gmail.com>
2018-10-31 09:12:07 +0100

    EIB-002: Classes and methods (basic). Phase 2

--- src/main/java/com/ctk/dao/Settings.java
+++ src/main/java/com/ctk/dao/Settings.java
@@ -8,9 +8,10 @@ public class Settings {
 
     private final Path pathLESPcsv = Paths.get(System.getProperty("jboss.server.data.dir"), "LESP.csv");
 
-    private Path getPathLESPcsv() {
+    public Path getPathLESPcsv() {
         return pathLESPcsv;
     }
+    
     private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public DateTimeFormatter getDateFormat() {
