16a3274080b835120779f8e41c8493984d4ce905
kurowskicisa <kurowskic.isa@gmail.com>
2019-10-22 08:50:14 +0200

    EIB-013: ElectronicInBox navigation. Phase 2

--- src/main/java/com/ctk/api/ElectronicInBoxRedirect.java
+++ src/main/java/com/ctk/api/ElectronicInBoxRedirect.java
@@ -14,7 +14,19 @@ public class ElectronicInBoxRedirect {
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
                 "\n" +
                 "    <meta http-equiv = \"refresh\" content =\"0, /electronicinbox/eib\">\n" +
+                "<title>Elektroniczne Skrzynki Podawcze</title>" +
                 "</head>" +
+                "<body>" +
+                "<div class=\"position-sticky sticky-top\">\n" +
+                "<nav class=\"navbar navbar-light bg-light\">\n" +
+                "<span class=\"navbar-brand mb-3 h1\">\n" +
+                "<a href=\"/electronicinbox/\"\n" +
+                "style=\"color: rgb(000, 000, 000); text-decoration: none; display: block;\">\n" +
+                "Elektroniczne Skrzynki Podawcze\n" +
+                "</a>\n" +
+                "</span>\n" +
+                "</nav>\n" +
+                "</body>" +
                 "</html>";
         return electronicInBoxRedirect;
     }
